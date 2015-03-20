4a8d2a344a9d18cdfbe9747b0b42a9492b3d3df6
Giorgos Logiotatidis <seadog@sealabs.net>
2015-03-20 21:39:04 +0200

    Fix static file serving.

--- redcalendar/events/models.py
+++ redcalendar/events/models.py
@@ -6,13 +6,11 @@ from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
 
-poster_storage = FileSystemStorage(location='./events/posters')
-
 class Event(models.Model):
     event_name = models.CharField(max_length=200)
     event_date = models.DateTimeField('date')
     event_place = models.CharField(max_length=100)
-    event_poster = models.ImageField(storage=poster_storage)
+    event_poster = models.ImageField(upload_to='posters')
 
     def __str__(self):
         return self.event_name
--- redcalendar/redcalendar/urls.py
+++ redcalendar/redcalendar/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
-#from . import views
-urlpatterns = [
+
+
+urlpatterns =  patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^events/', include('events.urls')),
+)
 
-    
-    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
